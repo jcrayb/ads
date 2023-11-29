32405770d50fdcac584e6b900a9f0edc7a0bc51a
jcrayb <jcrb7800@gmail.com>
2023-11-29 13:08:07 -0600

    updated routes js

--- static/ads.js
+++ static/ads.js
@@ -1,5 +1,5 @@
 function insert_ad(div_id){
-    fetch("http://localhost:8080/fetch/generate", {
+    fetch("/fetch/generate", {
         method: 'GET'
     })
     .then(response => response.json())
@@ -17,7 +17,7 @@ function get_ad_content(ad_id, div_id){
     height = div.clientHeight;
     width = div.clientWidth;
     console.log(height, width)
-    fetch(`http://localhost:8080/fetch/ad/${ad_id}?h=${height}&w=${width}`, {
+    fetch(`/fetch/ad/${ad_id}?h=${height}&w=${width}`, {
         method: 'POST'
     })
     .then(response => response.json())
@@ -25,7 +25,7 @@ function get_ad_content(ad_id, div_id){
         console.log(data)
         document.getElementById(div_id).innerHTML = `
         <a class="text-decoration-none text-black"
-        href="http://localhost:8080/fetch/redirect/${ad_id}?website=${window.location.host}">`+data['code']+"</a>"
+        href="/fetch/redirect/${ad_id}?website=${window.location.host}">`+data['code']+"</a>"
     }
     ).catch(error=>{
         console.error(error)
