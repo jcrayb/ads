d69d167c8ab8e7bff58f52771a8d766432f92d0d
jcrayb <jcrb7800@gmail.com>
2023-11-29 17:39:05 -0600

    images

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fetch.views import fetch
 
 from flask import Flask, render_template
+from init import initalize
 
 app = Flask(__name__)
 app.register_blueprint(fetch)
@@ -18,4 +19,5 @@ def main():
     return render_template("test.html")
 
 if __name__ == "__main__":
+    initalize()
     app.run(host="0.0.0.0", port="8080", debug=True)
--- init.py
+++ init.py
@@ -3,19 +3,32 @@ import sqlite3 as sq3
 con = sq3.connect('./db/ad.db', check_same_thread=False)
 cur = con.cursor()
 
-try:
-    cur.execute('''CREATE TABLE visits(
-            id VARCHAR,
-            date VARCHAR,
-            origin VARCHAR
-        )''')
-except:
-    pass
 
-try:
-    cur.execute('''CREATE TABLE ads(
-            id VARCHAR,
-            url VARCHAR
-        )''')
-except:
-    pass
+def initalize():
+    try:
+        cur.execute('''CREATE TABLE visits(
+                id VARCHAR,
+                date VARCHAR,
+                origin VARCHAR
+            )''')
+    except:
+        pass
+
+    try:
+        cur.execute('''CREATE TABLE ads(
+                id VARCHAR,
+                url VARCHAR
+            )''')
+    except:
+        pass
+
+    if not cur.execute('SELECT * FROM ads WHERE id="1"').fetchone():
+        cur.execute('INSERT INTO ads VALUES ("1", "https://jcrayb.com")')
+        print("1")
+
+    if not cur.execute('SELECT * FROM ads WHERE id="6d2818f3"').fetchone():
+        cur.execute('INSERT INTO ads VALUES ("6d2818f3", "https://jcrayb.com")')
+        print("6d2818f3")
+    
+    con.commit()
+    return
